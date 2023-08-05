93fbf6d117850373deb6226f226e0249d592a2dc
Abishek7592 <141495962+Abishek7592@users.noreply.github.com>
2023-08-05 20:12:37 +0530

    Add files via upload

---
+++ MERN-STATCK/FrontEnd/reactpp/src/components/interface.js
@@ -0,0 +1,20 @@
+
+
+
+import React from 'react'
+
+const Inter = () => {
+  return (
+   
+    <div className='text-2xl'>
+      <p className="cd">MERN STACK</p>
+      <div className='animate-bounce'>
+        
+      </div>
+     </div>
+     
+     
+  )
+}
+
+export default Inter
